25df62929b3cfaaa91a996147554b5dac9e1fef4
Coded-Error <67725633+Coded-Error@users.noreply.github.com>
2021-06-09 17:30:55 +0800

    Created Index

---
+++ java/base.js
@@ -0,0 +1,8 @@
+// Variables
+var thumbnailApi = 'https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=#USERID&size=#SIZE&format=Png&isCircular=#CIRCULAR'
+
+function Load() {
+}
+
+// Connection
+onload = Load
