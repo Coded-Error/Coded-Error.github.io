67a4d569fea2c571275923c96a8f50ed99e88c20
Coded-Error <67725633+Coded-Error@users.noreply.github.com>
2021-06-22 16:25:56 +0800

    - Social Media Link Class Overhaul - SAdded 3 Other Social Media Icons - Copy-Code-Clipboard FUnctionality

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,6 +2,7 @@
 import * as GitHub from './GitHubApis.js';
 
 // Functions
+/// Fills the '.repo' class using GitHub's API
 function FillRepositories() {
 	GitHub.GetRepositories()
 		.then(data => {
@@ -35,11 +36,11 @@ function isHidden(element) {
 	return element.is(":visible")
 }
 
-/// Toggles the 'repositories' class when the 'navbar:github' button is clicked
+/// Toggles the '.repositories' class when the 'navbar:github' button is clicked
 function OnGithubNavbarClick() {
 	$(".repositories").toggle()
 }
-/// Activates if the mouse clicks outside the .repositories element
+/// Activates if the mouse clicks outside the '.repositories' element
 function OnOutsideGithubClick(e) {
 	// Element
 	var element = $(".repositories")
@@ -52,21 +53,56 @@ function OnOutsideGithubClick(e) {
 	}
 }
 
+/// Adds a '<span'> tag to all '.smedia' classes
+function AddMediaTags() {
+	var _class = $(".smedia")
+
+	_class.each((_, _elem) => {
+		// Covert DOM Element to JQuery Element
+		_elem = $(_elem)
+
+		// Create add-on HTML; add it
+		var _html = $('<span>' + _elem.attr("content") + '</span>')
+		_elem.append(_html)
+	})
+}
+
+function ApplyCopyEvent(_, _elem) {
+	// Convert
+	_elem = $(_elem)
+
+	// Variables
+	var _html = $(`<span class="copy-button" title="Copy To Clipboard"></span>`)
+
+	_elem.append(_html)
+	/// This actually Copies to the Clipboard
+	_html.click(() => {
+		var text = _elem.text(); // Get the Text
+		navigator.clipboard.writeText(text) // Prevously an unnecessary large block of code: shortened to a single line
+	})
+}
+
+/// Activates once the document loads
 function OnDocumentLoad() {
 
 	// Variables
 	/// Elements
-	var _githubNavbar = document.getElementById("navbar:github")
+	var githubNavbar = $('#github-button')
+	var codeBlocks = $('.code-block')
 
 	// Set Visibility
 	/// Hide
 	$(".repositories").hide()
-	// Connections
-	_githubNavbar.onclick = OnGithubNavbarClick;
-	$(document).mouseup(OnOutsideGithubClick)
 
 	// Add Data
 	FillRepositories()
+	AddMediaTags()
+
+	// Connections
+	$(document).mouseup(OnOutsideGithubClick)
+	githubNavbar.click(OnGithubNavbarClick)
+	/// Apply the event for each '.code-block'
+	codeBlocks.each(ApplyCopyEvent)
 
 }
 
